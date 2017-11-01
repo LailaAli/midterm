7fdcb9ac2ffea5d525f7900c25a98e9321a563d5
Patrick Sollars <pjsollars@gmail.com>
2017-11-01 12:51:34 -0400

    adding js psuedocode

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,44 @@
 $(function() {
 
+// prompt user for budget amount
+
+// grab element meter id="budget" 
+// set val to current budget level
+
+// grab "add new" button 
+// .on("click" ----pop up new purchase window---)
+// class="new-purchase" set to display block
+
+//
+// get value from all form elements
+// id="purchase-new" value=""
+// id="purchase-amount" value=""
+// id="purchase-category" value="">
+// 	<option id="optionSelect" value="select">Select</option>
+// 	<option value="food">Food</option>
+// 	<option value="bills">Bills</option>
+// 	<option value="clothing">Clothing</option>
+// 	<option value="entertainment">Entertainment</option>
+// 	<button id="save" class="form-btn" type="button">Save</button>
+
+// check form values, they shouldn't be empty or select
+
+
+// set new purchase window to display none
+
+
+// update category levels
+// id="entertainment-spend"
+// id="food-spend"
+// id="clothing-spend"
+// id="bills-spend"
+
+
+
+// if budget val(0)
+// alert("You've spent your budget, no more buying!!");
+
+
+
+
 }); // wrapper
-
-
-
-
