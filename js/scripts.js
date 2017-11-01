39ccb5536f974c073774fcf3c932b533136b1802
Patrick Sollars <pjsollars@gmail.com>
2017-11-01 16:01:14 -0400

    initial prompt function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,40 @@
-$(function() {
+//$(function() {
 
 // prompt user for budget amount
+const initialPrompt = function() {
+	let budgetPrompt = prompt("Please enter your weekly budget amount");
+	let budgetAmount = parseInt(budgetPrompt);
+	if (isNaN(budgetAmount)) {
+		alert("You must enter a number.");
+		initialPrompt();
+	}
+	initializeBudget(budgetAmount);
+};
+
+const initializeBudget = function(budgetAmount) {
+	alert(budgetAmount);
+	$("#total").text(budgetAmount);
+	$("#remaining").text(budgetAmount);
+	let meter = $("#budget");
+
+	// budgetAmount = parseInt(budgetPrompt);
+	// if (budgetAmount === NaN) {
+	// 	alert("You must enter a number.");
+	// 	initialPrompt();
+	// }
+	// return budgetAmount;
+	// call initialize function and pass budget amount
+};
+// initialize budget amount variable
+// set these things to budget amount
+// <span id="total"></span>
+// <meter max="500"
+// value="375"
 
 // grab element meter id="budget" 
 // set val to current budget level
+
+
 
 // grab "add new" button 
 // .on("click" ----pop up new purchase window---)
@@ -13,7 +44,6 @@ $(function() {
 // get value from all form elements
 // id="purchase-new" value=""
 // id="purchase-amount" value=""
-<<<<<<< Updated upstream
 // id="purchase-category" value="">
 // 	<option id="optionSelect" value="select">Select</option>
 // 	<option value="food">Food</option>
@@ -21,15 +51,6 @@ $(function() {
 // 	<option value="clothing">Clothing</option>
 // 	<option value="entertainment">Entertainment</option>
 // 	<button id="purchase-save" class="form-btn" type="button">Save</button>
-=======
-// id="purchase-category" value=""
-// 	value="food">Food
-// 	value="bills">Bills
-// 	value="clothing">Clothing
-// 	value="entertainment">Entertainment
-// id="save" .on("click")
-// class="form-btn" type="button">Save</button>
->>>>>>> Stashed changes
 
 // check form values, they shouldn't be empty or select
 
@@ -53,6 +74,5 @@ $(function() {
 // alert("You've spent your budget, no more buying!!");
 
 
-
-
-}); // wrapper
+window.setTimeout(initialPrompt, 500);
+//}); // wrapper
